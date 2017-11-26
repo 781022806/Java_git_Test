2d2e6ee4186afd9350e122b4220d3766c8662fd2
如初 <781022806@qq.com>
2017-11-26 23:49:13 +0800

    Update Woaiyangbaobao.java

--- src/com/atguigu/main/Woaiyangbaobao.java
+++ src/com/atguigu/main/Woaiyangbaobao.java
@@ -4,5 +4,14 @@ package com.atguigu.main;
 public class Woaiyangbaobao {
 	public static void main(String[] args) {
 		System.out.println("我爱你啊  宝贝");
+		
+		
+		System.out.println("我爱你啊  宝贝10");
+		
+		System.out.println("我爱你啊  宝贝10");
+		
+		System.out.println("我爱你啊  宝贝10");
+		
+		System.out.println("我爱你啊  宝贝10");
 	}
 }
