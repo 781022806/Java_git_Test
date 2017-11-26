29a22ffcf1181879fe6d8929a9e45a39f01343b6
ruchu <781022806@qq.com>
2017-11-26 23:50:34 +0800

    宝贝在找裤子

--- src/com/atguigu/main/Woaiyangbaobao.java
+++ src/com/atguigu/main/Woaiyangbaobao.java
@@ -5,13 +5,5 @@ public class Woaiyangbaobao {
 	public static void main(String[] args) {
 		System.out.println("我爱你啊  宝贝");
 		
-		
-		System.out.println("我爱你啊  宝贝10");
-		
-		System.out.println("我爱你啊  宝贝10");
-		
-		System.out.println("我爱你啊  宝贝10");
-		
-		System.out.println("我爱你啊  宝贝10");
 	}
 }
